06d86dc337733ee2a160184b4d45075bb8d1577d
Likith <likithsrinath2000@gmail.com>
2020-09-02 14:37:12 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector(".result").innerHTML = "🥇Player 1 Wins!🥈";
+}
+
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector(".result").innerHTML = "🥈Player 2 Wins!🥇";
+}
+
+else {
+    document.querySelector(".result").innerHTML = "Draw!";
+}
